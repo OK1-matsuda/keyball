d4caaf9e49a4348167861326d6887c2012c04b99
Matsuda <124767003+OK1-matsuda@users.noreply.github.com>
2026-05-06 04:07:47 +0900

    Auto Mouseのデフォルトレイヤーを設定し、戻る時間を1500msに変更

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -42,12 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
-// Enable Auto Mouse feature at keyboard level so lib/keyball is built with
-// auto-mouse support available.  Keymap-level `config.h` is too late for
-// compiling the shared library, so define these here.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 4
-#define AUTO_MOUSE_TIME 2000 // ms: time to return to original layer
 
 // RGB LED settings
 #define WS2812_DI_PIN       D3
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mod_via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mod_via/config.h
@@ -36,6 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 4
+#define AUTO_MOUSE_TIME 1500 // ms: time to return to original layer
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 #define KEYBALL_SCROLL_DIV_DEFAULT 2
